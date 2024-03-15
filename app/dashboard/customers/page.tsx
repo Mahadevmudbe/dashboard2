0b05e427b41c7799869c292448e195254b8fda40
unknown <mudbemahadev0@gmail.com>
2024-03-15 16:03:05 +0530

    third

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
-export default function (){
+export default function page(){
     return <p>customer page</p>
 }
--- app/dashboard/invoice/page.tsx
+++ app/dashboard/invoice/page.tsx
@@ -1,3 +1,3 @@
-export default function (){
+export default function page(){
     return <p>invoice page</p>
 }
